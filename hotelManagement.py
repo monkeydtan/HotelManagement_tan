cc845101be143fc002bf6cc4e140a4784109ea5e
Monkeydtan <gamger12@gmail.com>
2024-10-03 11:53:19 +0700

    2nd commit

--- hotelManagement.py
+++ hotelManagement.py
@@ -7,6 +7,7 @@ class Room:
         self.price = price
         self.is_available = is_available # เอาไว้เช็คว่าห้องนั้นว่างไหม ค่าเริ่มต้นเป็น False คือ ยังไม่ถูกจอง หรือ ว่าง
     
+    # ฟังก์ชันการจองห้องพัก **ฟังก์ชันนี้จะถูกเอาไปใช้ตอนที่ลูกค้าจะทำการจองห้องพัก**
     def booked_room(self):
         if not self.is_available: # ถ้า not false = true คือ ถูกจองแล้ว
             self.is_available = True # ถูกจองแล้ว
@@ -23,20 +24,33 @@ class Room:
         )
         
 class Customer:
-    def __init__(self,name,id_card,booked_rooms):
+    def __init__(self,name,id_card):
         self.name = name
         self.id_card = id_card # เลขบัตรปชช.
-        self.booked_rooms = booked_rooms # รายการการจองห้องพัก
+        self.booked_rooms = [] # รายการการจองห้องพัก เก็บไว้เป็น List จะได้เก็บไว้หลายๆห้อง
+    
+    # ฟังก์ชันสำหรับการจองห้องพักของลูกค้า
+    def book_room(self,room):
+        if not room.is_available: # ห้องยังว่างไหม คือ ว่าง=False ซึ่งถ้ายังว่างจะเข้า loop การจอง ทำให้ค่า is_available กลายเป็น True
+            room.booked_room() #เรียกใช้ method การจองห้องจากคลาส Room ซึ่งก็คือ booked_room
+            self.booked_rooms.append(room) # เพิ่มห้องที่มีการจองแล้ว ไว้ใน List
+            return f"{self.name} ได้จองห้อง {room.room_number} เรียบร้อยแล้ว"
+        else:
+            return f"ห้อง {room.room_number} ถูกจองไปแล้ว"
 
-room1 = Room(101,"Standard",590)
-room1.is_available
-room1.booked_room()
-print(room1)
+    def __str__(self):
+        status_of_cus = "ยังไม่ได้ทำการจอง" if not self.booked_rooms else "จองแล้ว"
+        return (f"รายละเอียดผู้เข้าพัก\n"
+                f"ชื่อ: {self.name}\n"
+                f"หมายเลขบัตรประชาชน: {self.id_card}\n"
+                f"รายการห้องที่จอง: {status_of_cus}\n"
+        )
+
       
-# class Hotel:
-#     def __init__(self):
-#         self.rooms = [] # เอาไว้เก็บห้องทั้งหมดในโรงแรม
-#         self.customers = [] # เอาไว้เก็บรายชื่อลูกค้าของโรงแรม
+class Hotel:
+    def __init__(self):
+        self.rooms = [] # เอาไว้เก็บห้องทั้งหมดในโรงแรม
+        self.customers = [] # เอาไว้เก็บรายชื่อลูกค้าของโรงแรม
     
 #     # ฟังก์ชันการเพิ่มห้องพัก 
 #     def add_room(self,amount_room):
@@ -55,3 +69,19 @@ print(room1)
         
 #     # ฟังก์ชันสำหรับการเช็คเอาท์ห้องพัก
 #     # def checkout_room(self):
+
+# สร้าง object ห้อง กับ object ลูกค้า
+room1 = Room(101,"Standard",590)
+room2 = Room(201,"deluxe",790)
+
+# ลูกค้าทำการจองห้อง
+customer1 = Customer("nisakorn",123456789)
+customer1.book_room(room1) # จองห้องพัก
+customer2 = Customer("Tantan",2699562616)
+
+# สถานะลูกค้า
+print(customer1)
+print(customer2)
+# สถานะห้อง
+print(room1)
+print(room2)
