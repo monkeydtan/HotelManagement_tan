111809c1588548cf7069be3a9e6cb29a2980f04a
Monkeydtan <gamger12@gmail.com>
2024-10-03 12:30:57 +0700

    3rd commit modify more

--- hotelManagement.py
+++ hotelManagement.py
@@ -27,23 +27,12 @@ class Customer:
     def __init__(self,name,id_card):
         self.name = name
         self.id_card = id_card # เลขบัตรปชช.
-        self.booked_rooms = [] # รายการการจองห้องพัก เก็บไว้เป็น List จะได้เก็บไว้หลายๆห้อง
-    
-    # ฟังก์ชันสำหรับการจองห้องพักของลูกค้า
-    def book_room(self,room):
-        if not room.is_available: # ห้องยังว่างไหม คือ ว่าง=False ซึ่งถ้ายังว่างจะเข้า loop การจอง ทำให้ค่า is_available กลายเป็น True
-            room.booked_room() #เรียกใช้ method การจองห้องจากคลาส Room ซึ่งก็คือ booked_room
-            self.booked_rooms.append(room) # เพิ่มห้องที่มีการจองแล้ว ไว้ใน List
-            return f"{self.name} ได้จองห้อง {room.room_number} เรียบร้อยแล้ว"
-        else:
-            return f"ห้อง {room.room_number} ถูกจองไปแล้ว"
 
     def __str__(self):
-        status_of_cus = "ยังไม่ได้ทำการจอง" if not self.booked_rooms else "จองแล้ว"
-        return (f"รายละเอียดผู้เข้าพัก\n"
-                f"ชื่อ: {self.name}\n"
+        #status_of_cus = "ยังไม่ได้ทำการจอง" if not self.booked_rooms else "จองแล้ว"
+        return (f"ชื่อ: {self.name}\n"
                 f"หมายเลขบัตรประชาชน: {self.id_card}\n"
-                f"รายการห้องที่จอง: {status_of_cus}\n"
+                # f"รายการห้องที่จอง: {status_of_cus}\n"
         )
 
       
@@ -51,37 +40,55 @@ class Hotel:
     def __init__(self):
         self.rooms = [] # เอาไว้เก็บห้องทั้งหมดในโรงแรม
         self.customers = [] # เอาไว้เก็บรายชื่อลูกค้าของโรงแรม
+        self.booked_rooms = [] # รายการการจองห้องพัก เก็บไว้เป็น List จะได้เก็บไว้หลายๆห้อง
     
 #     # ฟังก์ชันการเพิ่มห้องพัก 
-#     def add_room(self,amount_room):
-#         self.rooms.append(amount_room)
+    def add_room(self,room):
+        self.rooms.append(room)
 
-#     # ฟังก์ชันสำหรับแสดงห้องว่างทั้งหมด
-#     def show_available_rooms(self):
-#         rooms_details = "\n".join(str(room_number) for room_number in self.self.rooms)
-#         customer_details = "\n".join(str(name) for name in self.customers)
-#         return f"ห้องว่างทั้งหมด : \n{rooms_details}"
+    # ฟังก์ชันสำหรับการจองห้องพักของลูกค้า
+    def book_room(self,customer,room):
+        if not room.is_available: # ห้องยังว่างไหม คือ ว่าง=False ซึ่งมี not ทำให้กลายเป็น not False หรือ True หมายถึงห้องไม่ว่างหรือถูกจองไปแล้ว
+            return f"ห้อง {room.room_number} ถูกจองไปแล้ว"
+        else: # ถ้ายังว่าง
+            room.booked_room() #เรียกใช้ method การจองห้องจากคลาส Room ซึ่งก็คือ booked_room
+            self.booked_rooms.append(room) # เพิ่มห้องที่มีการจองแล้ว ไว้ใน List
+            return f"{customer.name} ได้จองห้อง {room.room_number} เรียบร้อยแล้ว"
+
+# ฟังก์ชันสำหรับแสดงห้องว่างทั้งหมด
+    def show_available_rooms(self):
+        available_rooms = [str(room) for room in self.rooms if not room.is_available]  # ตรวจสอบห้องว่าง
+        if available_rooms:
+            return f"รายละเอียดห้องว่าง: \n" + "\n".join(available_rooms)
+        else:
+            return "ไม่มีห้องว่าง"
+  
         
-        
-#     # ฟังก์ชันสำหรับให้ลูกค้าจองห้องพัก
-#     def book_room(self,customer,number,type):
-        
-        
-#     # ฟังก์ชันสำหรับการเช็คเอาท์ห้องพัก
-#     # def checkout_room(self):
 
 # สร้าง object ห้อง กับ object ลูกค้า
 room1 = Room(101,"Standard",590)
 room2 = Room(201,"deluxe",790)
 
-# ลูกค้าทำการจองห้อง
+# สร้าง object ลูกค้า
 customer1 = Customer("nisakorn",123456789)
-customer1.book_room(room1) # จองห้องพัก
 customer2 = Customer("Tantan",2699562616)
 
+# สร้าง object โรงแรม
+hotel = Hotel()
+#--------- เพิ่มห้องเข้าไปใน List -----------#
+hotel.add_room(room1)
+hotel.add_room(room2)
+
+#--------- แสดงรายการห้องว่างก่อนการจอง ------------#
+print(hotel.show_available_rooms())
+ 
+#--------- ลูกค้าทำการจองห้อง ------------#
+print(hotel.book_room(customer1,room1))
+
 # สถานะลูกค้า
-print(customer1)
-print(customer2)
+# print(customer1)
+# print(customer2)
+
 # สถานะห้อง
-print(room1)
-print(room2)
+# print(room1)
+# print(room2)
